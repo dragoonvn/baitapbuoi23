608ecacb592e64adfde0d81f73fed39c705bbf9a
dragoonvn <trietvm@hotmail.com>
2020-06-15 16:54:38 +0700

    Activity-20

---
+++ src/lession3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class lession3 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số a: ");
+        a = scanner.nextInt();
+        int i = 1;
+        while (i <= 10) {
+            b = a * i;
+            System.out.println(a + " x " + i + " = " + b);
+            i++;
+        }
+    }
+}
